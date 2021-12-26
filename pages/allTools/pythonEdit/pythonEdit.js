78ace45ed2c8ba706d778df3ccf332aaa5b86633
林景本地端 <1986705965@qq.com>
2021-12-26 11:22:22 +0800

    完善blog页面图片以及代码显示,修改部分bug 1.1.7

--- pages/allTools/pythonEdit/pythonEdit.js
+++ pages/allTools/pythonEdit/pythonEdit.js
@@ -4,27 +4,65 @@ Page({
 
   data: {
     article:{},//代码内容
+    content:"其实一开始我就给内容了",
+    placeHolder:"代码编辑区域,点击此处进行编辑",
+    focusIsEdit:"dontShow",
+    toBottom:0,
+    isFoucs:false
   },
 
   onLoad: function (options) {
-    let result=app.towxml("def somefunc():","html",{
+    // let result=app.towxml("def somefunc():","html",{
       // theme:"dark",
-      events:{					// 为元素绑定的事件方法
-        tap:(e)=>{
-          console.log('tap',e);
-        }
+      // events:{					// 为元素绑定的事件方法
+      //   tap:(e)=>{
+      //     console.log('tap',e);
+      //   }
+      // }
+  // // })
+  //   this.setData({
+  //     article:result
+  //   })
+  },
+  keyboardChange(e) {
+    if(e.detail.height ==0){
+      this.setData({
+        focusIsEdit:"dontShow",
+      })
+    }
+    else{
+      if (this.data.toBottom >e.detail.height){
+        this.setData({
+          focusIsEdit:"show",
+        })
       }
-  })
-    this.setData({
-      article:result
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
+      else{
+        this.setData({
+          focusIsEdit:"show",
+          toBottom:e.detail.height
+        })
+      }
+    }
+    },
+    // 缩进
+    focusEdit(e){
+      console.log(e)
+      this.setData({
+        focusIsEdit:"show",
+      })
+    },
+    blurEdit(e){
+      // console.log(e.detail.value)
+      this.setData({
+        focusIsEdit:"dontShow",
+        content:e.detail.value
+      })
+    },
+    makeTap(e){
+      console.log(app.globalData.content)
+      this.setData({
+        content:"尼玛死了",
+        isFoucs:true,
+      })
+    },
 })
--- pages/toolHome/tool.js
+++ pages/toolHome/tool.js
@@ -6,7 +6,7 @@ Page({
       {"toolImg":"../../images/pdf.png","toolName":"转PDF","goPage":"pdfChange"},
       {"toolImg":"../../images/writeBlog.png","toolName":"博客交流","goPage":"blogIndex"},
       {"toolImg":"../../images/Python.png","toolName":"python文档","goPage":"pythonDocIndex"},
-      {"toolImg":"../../images/pythonEdit.png","toolName":"在线编程","goPage":"pythonEdit"},
+      {"toolImg":"../../images/pythonEdit.png","toolName":"在线编程","goPage":"/"},
       {"toolImg":"../../images/json.png","toolName":"JSON解析器","goPage":"/"},
       {"toolImg":"../../images/MD5.png","toolName":"MD5加密","goPage":"/"},
     ]
@@ -17,10 +17,10 @@ Page({
   goPage(e){
     console.log(e.currentTarget.dataset.page)
     if (app.globalData.userInfo ==null){
-      // this.getUserProfile(e.currentTarget.dataset.page)
-      wx.redirectTo({
-        url:"../allTools/"+e.currentTarget.dataset.page+"/"+e.currentTarget.dataset.page,
-      })
+      this.getUserProfile(e.currentTarget.dataset.page)
+      // wx.redirectTo({
+      //   url:"../allTools/"+e.currentTarget.dataset.page+"/"+e.currentTarget.dataset.page,
+      // })
     }
     else{
       if (e.currentTarget.dataset.page =="/"){
